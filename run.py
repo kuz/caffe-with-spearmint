6530fe2ec7a6f7a34a6ad882ea327f22b7ab0b75
kuz <ilya.kuzovkin@gmail.com>
2015-07-22 19:31:39 +0300

    Added check that experiment directory structure is in place

--- run.py
+++ run.py
@@ -3,6 +3,7 @@ import string
 from copy import copy
 from cwsm.spearmint import ConfigFile
 import subprocess
+import os
 
 # command line arguments
 parser = argparse.ArgumentParser(description='Run hyperparameter seacrh for a caffe model.')
@@ -11,9 +12,42 @@ args = parser.parse_args()
 netfile = args.experiment + '/model/trainval.prototxt'
 solverfile = args.experiment + '/model/solver.prototxt'
 
+# run checks that all the experiment folder structure is in place
+def ensurepath(path):
+    if not os.path.exists(path):
+        os.mkdir(path)
+        print 'Creating %s' % path
+
+def demandpath(path, message):
+    if not os.path.exists(path):
+        print message % path
+        exit()
+
+ensurepath(args.experiment + '/caffeout')
+ensurepath(args.experiment + '/data')
+ensurepath(args.experiment + '/model')
+ensurepath(args.experiment + '/mongodb')
+ensurepath(args.experiment + '/spearmint')
+ensurepath(args.experiment + '/tmp')
+
+demandpath(args.experiment + '/data/mean_train.binaryproto', 'Please put mean files as %s')
+demandpath(args.experiment + '/data/train_lmdb', 'Please put your training LMDB as %s')
+demandpath(args.experiment + '/data/val_lmdb', 'Please put your validation LMDB as %s')
+demandpath(args.experiment + '/model/solver.prototxt', 'Your Caffe solver file should be located at %s')
+demandpath(args.experiment + '/model/trainval.prototxt', 'Your Caffe network description file should be located at %s')
+
 # read in caffe .prototxt files
 net = open(netfile, 'r').read()
 solver = open(solverfile, 'r').read()
+
+# check that solver has required placeholders
+if '"PLACEHOLDER_NET"' not in solver:
+    print 'Your solver.prototxt has to have "net: "PLACEHOLDER_NET"" line in it.'
+    exit()
+if '"PLACEHOLDER_MODEL_STORE"' not in solver:
+    print 'Your solver.prototxt has to have "snapshot_prefix: "PLACEHOLDER_MODEL_STORE"" line in it.'
+    exit()
+
 
 # inialize templates for output files
 tmpl_net = copy(net)
